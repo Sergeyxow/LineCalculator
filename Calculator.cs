262817ae208dfe5c856e121c571ca73ff8dc0d17
Sergey Khrulev <sergeyxow@yandex.ru>
2020-04-29 16:35:21 +0300

    Refactoring Tests No brackets support

---
+++ Calculator.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace LineCalculator
+{
+    public class Calculator
+    {
+        public Dictionary<char, int> OperatorsPriority = new Dictionary<char, int>
+        {
+            {'+', 1},
+            {'-', 1},
+            {'*', 2},
+            {'/', 2}
+        };
+
+        public Stack<float> Numbers = new Stack<float>();
+        public Stack<char> Operators = new Stack<char>();
+
+        public float Calculate(string line)
+        {
+            ParseLine(line);
+            //Console.WriteLine(Numbers.Count);
+            return Numbers.Pop();
+        }
+        
+        public void ParseLine(string line)
+        {
+            string num = String.Empty;
+            line = line.Replace(" ", "");
+            //Console.WriteLine(line);
+
+            foreach (char c in line)
+            {
+                if (Char.IsDigit(c) || c == '.')
+                {
+                    num += c;
+                }
+                else
+                {
+                    if (num != String.Empty)
+                    {
+                        Numbers.Push(ParseNumber(num));
+                        num = string.Empty;
+                    }
+
+                    if (IsCharOperator(c))
+                    {
+                        PositionOperator(c);
+                    }
+                    
+                }
+            }
+
+            if (num != String.Empty)
+            {
+                Numbers.Push(ParseNumber(num));
+                num = string.Empty;
+            }
+
+            int operatorsLeft = Operators.Count;
+            for (int i = 0; i < operatorsLeft; i++)
+            {
+                Numbers.Push(CalculateTwoLastNumbers(Operators.Pop()));
+            }
+            
+            //Console.WriteLine(Operators.Count);
+        }
+
+        public float ParseNumber(string number)
+        {
+            try
+            {
+                return float.Parse(number, CultureInfo.InvariantCulture.NumberFormat);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public bool IsCharOperator(char c)
+        {
+            if (c == '+' || c == '-' || c == '*' || c == '/')
+                return true;
+            return false;
+        }
+
+        public void PositionOperator(char op)
+        {
+            if (Operators.Count < 1)
+            {
+                Operators.Push(op);
+                return;
+            }
+                
+            char lastOperator = Operators.Peek();
+            
+            int lastOperatorPriority = OperatorsPriority[lastOperator];
+            int thisOperatorPriority = OperatorsPriority[op];
+
+            if (thisOperatorPriority > lastOperatorPriority)
+            {
+                Operators.Push(op);
+            }
+            else
+            {
+                lastOperator = Operators.Pop();
+                Numbers.Push(CalculateTwoLastNumbers(lastOperator));
+                Operators.Push(op);
+            }
+        }
+
+        public float CalculateTwoLastNumbers(char op)
+        {
+            float last = Numbers.Pop();
+            float preLast = Numbers.Pop();
+
+            float result = 0;
+            
+            switch (op)
+            {
+                case '+':
+                    result = preLast + last;
+                    break;
+                case '-':
+                    result = preLast - last;
+                    break;
+                case '*':
+                    result = preLast * last;
+                    break;
+                case '/':
+                    result = preLast / last;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ UnitTests/CalculatorTests.cs
@@ -0,0 +1,116 @@
+using LineCalculator;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class CalculatorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestCalculate()
+        {
+            // //arrange
+            // Calculator calculator = new Calculator();
+            // string line = "25.6 * 5 + 4 / 5 - 3 ";
+            // float expected = 25.6f * 5 + 4 / 5 - 3;
+            // float result;
+            
+            //By some reason C# calculates the line wrong
+            
+            //arrange
+            Calculator calculator = new Calculator();
+            string line = "45 / 3 * 85 - 4";
+            float expected = 45 / 3 * 85 - 4;
+            float result;
+            
+            //act
+            result = calculator.Calculate(line);
+            
+            //assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestParseNumber()
+        {
+            //arrange
+            Calculator calculator = new Calculator();
+            string num = "22.5";
+            float result;
+
+            //act
+            result = calculator.ParseNumber(num);
+            
+            //assert
+            Assert.AreEqual(22.5f, result);
+        }
+
+        [Test]
+        public void TestPlusCalculateTwoLastNumbers()
+        {
+            //arrange
+            Calculator calculator = new Calculator();
+            calculator.Numbers.Push(10);
+            calculator.Numbers.Push(13.5f);
+            float result;
+            
+            //act
+            result = calculator.CalculateTwoLastNumbers('+');
+            
+            //assert
+            Assert.AreEqual(10 + 13.5f, result);
+        }
+
+        [Test]
+        public void TestMinusCalculateTwoLastNumbers()
+        {
+            //arrange
+            Calculator calculator = new Calculator();
+            calculator.Numbers.Push(10);
+            calculator.Numbers.Push(13.5f);
+            float result;
+            
+            //act
+            result = calculator.CalculateTwoLastNumbers('-');
+            
+            //assert
+            Assert.AreEqual(10 - 13.5f, result);
+        }
+
+        [Test]
+        public void TestMultiplyCalculateTwoLastNumbers()
+        {
+            //arrange
+            Calculator calculator = new Calculator();
+            calculator.Numbers.Push(10);
+            calculator.Numbers.Push(13.5f);
+            float result;
+            
+            //act
+            result = calculator.CalculateTwoLastNumbers('*');
+            
+            //assert
+            Assert.AreEqual(10 * 13.5f, result);
+        }
+
+        [Test]
+        public void TestDivideCalculateTwoLastNumbers()
+        {
+            //arrange
+            Calculator calculator = new Calculator();
+            calculator.Numbers.Push(10);
+            calculator.Numbers.Push(13.5f);
+            float result;
+            
+            //act
+            result = calculator.CalculateTwoLastNumbers('/');
+            
+            //assert
+            Assert.AreEqual(10 / 13.5f, result);
+        }
+    }
+}
